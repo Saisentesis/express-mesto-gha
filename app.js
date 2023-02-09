5db2ecf54a18d0c4454877edfc2907cae351ce8e
Andrey Popov <psixozzzz@yandex.ru>
2023-02-09 10:59:44 +0500

    добавил логирование

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const NotFoundError = require('./errors/NotFound');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const ErrorHandler = require('./errors/ErrorHandler');
 
 const { PORT = 3000 } = process.env;
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mydb');
 
+app.use(requestLogger);
 app.use('/', require('./routes/auth'));
 
 app.use(auth);
@@ -25,6 +27,7 @@ app.use('/', (req, res, next) => {
   next(new NotFoundError('Неправильный путь'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(ErrorHandler);
 
